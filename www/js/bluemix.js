a456315d6e3c917be99e949e85fc7591665a1cab
CalebBurton <Caleb@Calebs-MacBook-Air.local>
2016-07-15 16:57:19 -0400

    Audio Upload/Download Success Audio files can be uploaded, downloaded, and previewed. Testing confirms that actual audio files are being sent, not just the address of where an audio file is located on the host device.

--- www/js/bluemix.js
+++ www/js/bluemix.js
@@ -58,8 +58,7 @@ angular.module('myApp.bms', ['ionic'])
     var cloudant_bms_guid = "503a965b-5ba8-4a3d-b7e7-403375548369";
     // Global vars used for debugging and keeping track of which functions are being called
     var funct = "";
-    var testFile = new Image();
-    //var testFile = new Audio();
+    var audioFile;
     // Android Client ID: 853130241725-mtljp8gct4pqtjvvrdt1kl9e8t90vho3.apps.googleusercontent.com
 
     $scope.recordings = Recordings.all();
@@ -157,26 +156,15 @@ angular.module('myApp.bms', ['ionic'])
 
         funct = "CLOUDANT_UPLOAD";
         BMSClient.initialize(cloudant_bms_route, cloudant_bms_guid);
-        // Cloudant DB info
-        //
-        //{ Key: 'wilyhowersecoughtselaire', 
-        //  Password: 'b5a398755a7751978efd2637e57ce844bbd86a46'
-        //}
 
-        /*
-        var form = new FormData();
-	    form.append("file", file);
-            alert("Form created: ");
-            alert(form.formData);
-        */
 
         var cloudant_Username = "812cf44b-c59b-4288-a505-ad7e6b1b2f55-bluemix";
         var cloudant_Database = "my_sample_db";
         var cloudant_DocID = "9824ffba8c5837b1272a1fb08c96dec3";
         var cloudant_DocRev = "";               // Will be updated by pinging the server below
         var queryRev = "";
-        var cloudant_Attachment = "testFile.png"; //Date.now() + ".jpg";   // Ensures that each file name will be unique
-        var cloudant_MIMEtype = "image/png"; //audio/wav";
+        var cloudant_Attachment = Date.now() + ".wav";   // Ensures that each file name will be unique
+        var cloudant_MIMEtype = "audio/wav"; //"image/jpg";
         var senderURL =     "https://" + cloudant_Username + ".cloudant.com/" + cloudant_Database + "/" + cloudant_DocID + "/" + cloudant_Attachment;
         var requesterURL =  "https://" + cloudant_Username + ".cloudant.com/" + cloudant_Database + "/" + cloudant_DocID;
         
@@ -199,26 +187,9 @@ angular.module('myApp.bms', ['ionic'])
         
         setTimeout(
             function() {    // This function won't execute until the time below expires (gives the server time to respond)
-/*
-                var form = new FormData();
-	            form.append("file", testFile.src);
-                senderURL += queryRev; //"api/favorites" + "/attach?" + queryParams;
-                var xhr = new XMLHttpRequest();
-                xhr.open("PUT", senderURL, true);
-                //xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                xhr.onreadystatechange = function(){
-                    if(xhr.readyState == 4){
-                        alert("Server responded.\n\nStatus: " + xhr.status + "\n\nResponse: " + xhr.responseText);
-                    }
-                };
-                //xhr.timeout = 1000000;
-                //xhr.ontimeout = alert("Request timed out");
-                xhr.send(form);
-*/
-               
                 var sender = new MFPRequest(senderURL + queryRev, MFPRequest.PUT);
                 var headers = {"Content-Type": cloudant_MIMEtype};
-                var payload = testFile.src;
+                var payload = audioFile;
 
                 sender.setHeaders(headers);
                 sender.send(
@@ -234,17 +205,19 @@ angular.module('myApp.bms', ['ionic'])
         );
     };
 
-    $scope.debugger = function(){
+    $scope.displayer = function(){
         var f = document.getElementById('upload_file').files[0], r = new FileReader(); // Initializes a FileReader
         var preview = document.querySelector('#preview');
-        
         r.readAsDataURL(f);         // Uses the FileReader initialized above to actually read in the file
         r.addEventListener( "load", // Once the file has been read, do the following:
                             function () {
-                                testFile.height = 400; // only for images
-                                //testFile.type = cloudant_MIMEtype; // only for audio
-                                testFile.src = this.result;
-                                preview.appendChild(testFile);
+                                audioFile = this.result;
+                                preview.innerHTML =
+                                    "<audio controls>" +
+                                        "<source " +
+                                            "src=\"" + audioFile + "\"" +
+                                            "type=\"audio/wav\">" +
+                                    "</audio>";
                             },
                             false); // useCapture. A boolean that essentially assigns priority. FALSE is fine for our case.
     };
@@ -257,20 +230,21 @@ angular.module('myApp.bms', ['ionic'])
         var cloudant_DocID = "9824ffba8c5837b1272a1fb08c96dec3";
         var requesterURL =  "https://" + cloudant_Username + ".cloudant.com/"
                             + cloudant_Database + "/" + cloudant_DocID
-                            + "/" + "testFile.png";
+                            + "/" + "1468615697328.wav";
         
         var preview = document.querySelector('#preview');
         
         var requester = new MFPRequest(requesterURL, MFPRequest.GET);   // Ping the server to get the current database revision
         requester.send(
             function(successMsg) {  // Save the current revision so we can upload an attachment
-                var response64 = successMsg.responseText;
-                testFile.src = response64;
-                testFile.height = 300;
-                preview.appendChild(testFile);
-                alert("success?");
-                //var responseDecoded = Base64.decode(response64);
-                //alert("Response: " + successMsg.responseText); // NOTE: "_rev" WITH an underscore, unlike the response
+                alert("Ping received");
+                audioFile = successMsg.responseText;
+                preview.innerHTML =
+                    "<audio controls>" +
+                        "<source " +
+                            "src=\"" + audioFile + "\"" +
+                            "type=\"audio/wav\">" +
+                    "</audio>";
             },
             $scope.bms_failure
         );
