c8498ba18cf299e897bcbf29a93b2df8dff3a8a8
CalebBurton <Caleb@Calebs-MacBook-Air.local>
2016-07-15 17:18:03 -0400

    Cleaning and commenting Adding comments and refactoring code - COB 7/15/16

--- platforms/android/assets/www/js/bluemix.js
+++ platforms/android/assets/www/js/bluemix.js
@@ -56,10 +56,22 @@ angular.module('myApp.bms', ['ionic'])
     // Cloudant credentials
     var cloudant_bms_route = "http://ccb-cloudant.mybluemix.net";
     var cloudant_bms_guid = "503a965b-5ba8-4a3d-b7e7-403375548369";
+
     // Global vars used for debugging and keeping track of which functions are being called
     var funct = "";
     var audioFile;
     // Android Client ID: 853130241725-mtljp8gct4pqtjvvrdt1kl9e8t90vho3.apps.googleusercontent.com
+
+    var cloudant_Username = "812cf44b-c59b-4288-a505-ad7e6b1b2f55-bluemix";
+    var cloudant_Database = "my_sample_db";                                 // Will be the same accross all patients
+    var cloudant_DocID = "9824ffba8c5837b1272a1fb08c96dec3";                // Will be unique to each patient
+    var cloudant_DocRev = "";                                               // Will be updated by pinging the server below
+    var cloudant_Attachment = Date.now() + ".wav";                          // Named by time. Ensures that each file name will be unique
+    var cloudant_MIMEtype = "audio/wav"; //"image/jpg";
+    var preview = document.querySelector('#preview');                       // Grabs a <div> element. We'll modify it later to give a preview
+    
+    var senderURL =     "https://" + cloudant_Username + ".cloudant.com/" + cloudant_Database + "/" + cloudant_DocID + "/" + cloudant_Attachment;
+    var requesterURL =  "https://" + cloudant_Username + ".cloudant.com/" + cloudant_Database + "/" + cloudant_DocID;
 
     $scope.recordings = Recordings.all();
 
@@ -111,6 +123,7 @@ angular.module('myApp.bms', ['ionic'])
     };
 
     $scope.bms_deleteAndUpdate = function (todoID) {
+        BMSClient.initialize(cloudant_bms_route, cloudant_bms_guid);
         // Show the action sheet
         $ionicActionSheet.show({
             buttons: [
@@ -147,6 +160,7 @@ angular.module('myApp.bms', ['ionic'])
 
 
     $scope.cloudant_upload = function(file) {
+        BMSClient.initialize(cloudant_bms_route, cloudant_bms_guid);
         /*
         var recordingName = prompt("Please name the recording: ");
         if(recordingName) {
@@ -155,47 +169,25 @@ angular.module('myApp.bms', ['ionic'])
         */
 
         funct = "CLOUDANT_UPLOAD";
-        BMSClient.initialize(cloudant_bms_route, cloudant_bms_guid);
-
-
-        var cloudant_Username = "812cf44b-c59b-4288-a505-ad7e6b1b2f55-bluemix";
-        var cloudant_Database = "my_sample_db";
-        var cloudant_DocID = "9824ffba8c5837b1272a1fb08c96dec3";
-        var cloudant_DocRev = "";               // Will be updated by pinging the server below
-        var queryRev = "";
-        var cloudant_Attachment = Date.now() + ".wav";   // Ensures that each file name will be unique
-        var cloudant_MIMEtype = "audio/wav"; //"image/jpg";
-        var senderURL =     "https://" + cloudant_Username + ".cloudant.com/" + cloudant_Database + "/" + cloudant_DocID + "/" + cloudant_Attachment;
-        var requesterURL =  "https://" + cloudant_Username + ".cloudant.com/" + cloudant_Database + "/" + cloudant_DocID;
-        
-        /*
-        var queryID = "id=" + "testIDforQP";;
-	    var queryName = "&name=" + "testNameForQP";
-	    var queryValue ="&value=" + "testValueForQP";
-        var queryParams = queryID + queryName + queryValue;
-            alert("Query: " + queryParams);
-        */
 
         var requester = new MFPRequest(requesterURL, MFPRequest.GET);   // Ping the server to get the current database revision
         requester.send(
             function(successMsg) {  // Save the current revision so we can upload an attachment
-                cloudant_DocRev = JSON.parse(successMsg.responseText)._rev; // NOTE: "_rev" WITH an underscore, unlike the response
-                queryRev = "?rev=" + cloudant_DocRev;   // Our query needs to access the most up-to-date revision of this patient's data
+                cloudant_DocRev = JSON.parse(successMsg.responseText)._rev; // NOTE: "_rev" WITH an underscore, unlike the responseText
             },
             $scope.bms_failure
         );
         
-        setTimeout(
-            function() {    // This function won't execute until the time below expires (gives the server time to respond)
-                var sender = new MFPRequest(senderURL + queryRev, MFPRequest.PUT);
-                var headers = {"Content-Type": cloudant_MIMEtype};
+        setTimeout( // The wrapped function won't execute until the time below expires. This gives the server time to respond.
+            function() {
+                var sender = new MFPRequest(senderURL + "?rev=" + cloudant_DocRev, MFPRequest.PUT);
+                var headers = {"Content-Type": cloudant_MIMEtype};  // Probably not necessary
                 var payload = audioFile;
 
                 sender.setHeaders(headers);
                 sender.send(
                     payload,
                     function(successMsg) {
-                        alert("The CLOUDANT_UPLOAD function succeeded! Response:\n" + successMsg.responseText);
                         cloudant_DocRev = JSON.parse(successMsg.responseText).rev;  // NOTE: "rev" WITHOUT an underscore, unlike the sent message
                     },
                     $scope.bms_failure
@@ -206,34 +198,30 @@ angular.module('myApp.bms', ['ionic'])
     };
 
     $scope.displayer = function(){
-        var f = document.getElementById('upload_file').files[0], r = new FileReader(); // Initializes a FileReader
-        var preview = document.querySelector('#preview');
-        r.readAsDataURL(f);         // Uses the FileReader initialized above to actually read in the file
-        r.addEventListener( "load", // Once the file has been read, do the following:
+        BMSClient.initialize(cloudant_bms_route, cloudant_bms_guid);
+        var f = document.getElementById('upload_file').files[0] // Gives a convenient way to reference the file we uploaded
+        var r = new FileReader();                               // Initializes a FileReader
+        r.readAsDataURL(f);                                     // Uses the FilReader to actually read in the file
+        r.addEventListener( "load",                             // Once the file has been read, begin the following:
                             function () {
-                                audioFile = this.result;
+                                audioFile = this.result;        // This will be in the form of a DataURL
                                 preview.innerHTML =
                                     "<audio controls>" +
                                         "<source " +
                                             "src=\"" + audioFile + "\"" +
-                                            "type=\"audio/wav\">" +
+                                            "type=\"" + cloudant_MIMEtype + "\">" +
                                     "</audio>";
                             },
-                            false); // useCapture. A boolean that essentially assigns priority. FALSE is fine for our case.
+                            false); // useCapture. Essentially assigns priority for alerts. FALSE is fine for our case.
     };
 
     $scope.downloader = function() {
-        funct = "DOWNLOADER";
         BMSClient.initialize(cloudant_bms_route, cloudant_bms_guid);
-        var cloudant_Username = "812cf44b-c59b-4288-a505-ad7e6b1b2f55-bluemix";
-        var cloudant_Database = "my_sample_db";
-        var cloudant_DocID = "9824ffba8c5837b1272a1fb08c96dec3";
+        funct = "DOWNLOADER";
         var requesterURL =  "https://" + cloudant_Username + ".cloudant.com/"
                             + cloudant_Database + "/" + cloudant_DocID
                             + "/" + "1468615697328.wav";
-        
-        var preview = document.querySelector('#preview');
-        
+
         var requester = new MFPRequest(requesterURL, MFPRequest.GET);   // Ping the server to get the current database revision
         requester.send(
             function(successMsg) {  // Save the current revision so we can upload an attachment
@@ -243,7 +231,7 @@ angular.module('myApp.bms', ['ionic'])
                     "<audio controls>" +
                         "<source " +
                             "src=\"" + audioFile + "\"" +
-                            "type=\"audio/wav\">" +
+                            "type=\""+ cloudant_MIMEtype + "\">" +
                     "</audio>";
             },
             $scope.bms_failure
